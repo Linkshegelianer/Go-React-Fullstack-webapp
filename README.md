0f8d6740f6ed2681ffa7b7fed9cca795678c06c2
Lera <mssolipsismus@gmail.com>
2023-11-07 21:46:33 +0100

    add description + build

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
-# Go-React-Fullstack-App
-Web application for personal blog (Go/Gin, React, MySQL, Docker) 
+# Personal Blog Template
+
+This repository contains template for personal blog. This is an educational project aimed at the practice of working with Go/Gin, React, PostgreSQL and GitHub Actions.
+
+[![Go CI](https://github.com/Linkshegelianer/Go-React-Fullstack-webapp/workflows/Go%20CI/badge.svg)](https://github.com/Linkshegelianer/Go-React-Fullstack-webapp/actions/workflows/go-ci.yml)
+[![Maintainability](https://api.codeclimate.com/v1/badges/be241de1474774f51329/maintainability)](https://codeclimate.com/github/Linkshegelianer/Go-React-Fullstack-webapp/maintainability)
+[![Test Coverage](https://api.codeclimate.com/v1/badges/be241de1474774f51329/test_coverage)](https://codeclimate.com/github/Linkshegelianer/Go-React-Fullstack-webapp/test_coverage)
+
+##  Technologies
+#### Backend Technologies:
+1. Go server / Gin API router
+
+#### Frontend Technologies:
+1. React 17.0.2
+
+#### Database Management
+1. PostgreSQL
--- src/main/backend/main.go
+++ src/main/backend/main.go
@@ -1,6 +1,8 @@
 package backend
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func printNumbers1() {
 	for i := 0; i < 10; i++ {
@@ -31,3 +33,63 @@ func Add(x, y int) int {
 func main() {
 	fmt.Println("I'm working")
 }
+
+//func getLatLong(city string) (*LatLong, error) {
+//    endpoint := fmt.Sprintf("https://geocoding-api.open-meteo.com/v1/search?name=%s&count=1&language=en&format=json", url.QueryEscape(city))
+//    resp, err := http.Get(endpoint)
+//    if err != nil {
+//   	 return nil, fmt.Errorf("error making request to Geo API: %w", err)
+//    }
+//    defer resp.Body.Close()
+//
+//    var response GeoResponse
+//    if err := json.NewDecoder(resp.Body).Decode(&response); err != nil {
+//   	 return nil, fmt.Errorf("error decoding response: %w", err)
+//    }
+//
+//    if len(response.Results) < 1 {
+//   	 return nil, errors.New("no results found")
+//    }
+//
+//    return &response.Results[0], nil
+//}
+//
+//type GeoResponse struct {
+//    // A list of results; we only need the first one
+//    Results []LatLong `json:"results"`
+//}
+//
+//type LatLong struct {
+//    Latitude  float64 `json:"latitude"`
+//    Longitude float64 `json:"longitude"`
+//}
+//
+//func getWeather(latLong LatLong) (string, error) {
+//    endpoint := fmt.Sprintf("https://api.open-meteo.com/v1/forecast?latitude=%.6f&longitude=%.6f&hourly=temperature_2m", latLong.Latitude, latLong.Longitude)
+//    resp, err := http.Get(endpoint)
+//    if err != nil {
+//   	 return "", fmt.Errorf("error making request to Weather API: %w", err)
+//    }
+//    defer resp.Body.Close()
+//
+//    body, err := io.ReadAll(resp.Body)
+//    if err != nil {
+//   	 return "", fmt.Errorf("error reading response body: %w", err)
+//    }
+//
+//    return string(body), nil
+//}
+//
+//func main() {
+//    latlong, err := getLatLong("London") // you know it will rain
+//    if err != nil {
+//   	 log.Fatalf("Failed to get latitude and longitude: %s", err)
+//    }
+//    fmt.Printf("Latitude: %f, Longitude: %f\n", latlong.Latitude, latlong.Longitude)
+//
+//    weather, err := getWeather(*latlong)
+//    if err != nil {
+//   	 log.Fatalf("Failed to get weather: %s", err)
+//    }
+//    fmt.Printf("Weather: %s\n", weather)
+//}
